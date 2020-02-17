9a05c85621259d55ece727b9fc338c9330957ad0
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-02-17 18:33:13 -0500

    Selecting Days: after combined backup files

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,3 +3,4 @@ module.exports = {
   stories: ['../src/components/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
+
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,19 +4,29 @@ import React from "react";
 import "./DayListItem.scss";
 import classnames from 'classnames';
 
-
-// Don't forget to import DayListItem.scss into the DayListItem component before moving on.
-// Import the classnames library to the DayListItem component.
-// Create an object called dayClass that applies the day-list__item--selected class name if props.selected is true and the day-list__item--full class name if props.spots is 0.
 export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+
+  const formatSpots = function (originalFormat) {
+    if (originalFormat === 0) {
+      return "no spots remaining";
+    } else if (originalFormat === 1) {
+      return "1 spot remaining";
+    } else if (originalFormat === 2) {
+      return "2 spots remaining"
+    }
+    
+  }
+  const newPropSpots = formatSpots(props.spots);
+
   return (
     <li onClick={() => props.setDay(props.name)}>
       <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{props.spots}</h3>
+      <h3 className={dayClass}>{newPropSpots}</h3>
+      {/* <h3 className={dayClass}>{props.spots}</h3> */}
     </li>
   );
 }
