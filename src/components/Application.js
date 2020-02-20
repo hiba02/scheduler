fc3c6491b4455ac129f8dc35e4625dd97163ef93
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-02-20 09:05:27 -0500

    Managing State: need to fix removing warning

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,9 +44,19 @@ const appointments = [
 // ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState({...state, days});
+  // setState(prev => ({ ...prev, days }));
+  // console.log('state, setState', state, setState)
   const listOfAppointments = appointments.map(appointment => <Appointment 
     key={appointment.id} 
     id={appointment.id} 
@@ -59,7 +69,9 @@ export default function Application(props) {
 
     Axios.get(`http://localhost:8001/api/days`)
     .then((response) => {
-      setDays(response.data)
+      // console.log(response.data)
+      setDays(response.data);
+      
     });
   },[]);  
 
@@ -76,7 +88,7 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days} day={day} setDay={setDay} 
+          days={state.days} day={state.day} setDay={setDay} 
         />
       </nav>
       <img
