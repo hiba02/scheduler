20b330f63764397aeb4f96ec256129746e25eef6
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-03-03 07:38:50 -0500

    tiny change

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,20 +1,9 @@
 import React from "react";
-// import "./DayListItem.scss";
 import DayListItem from "./DayListItem"
 
 
-
 export default function DayList(props) {
-  // console.log('DayList props', props);
-  // console.log('DayList props days', props.days);
- /* 
- prop.days is...
- console.log('DayList props days', props.days)
-  (3) [{…}, {…}, {…}]
-0: {id: 1, name: "Monday", spots: 2}
-1: {id: 2, name: "Tuesday", spots: 5}
-2: {id: 3, name: "Wednesday", spots: 0}
-  */
+
   const dayList = props.days.map(day => 
 
    
